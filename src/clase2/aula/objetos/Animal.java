41a4ccc7ca543415dd6256732d37a18bb26f3ee1
PFR <PFR@LAB605-04>
2013-08-11 09:43:50 -0500

    Probando

---
+++ src/clase2/aula/objetos/Animal.java
@@ -0,0 +1,50 @@
+
+package clase2.aula.objetos;
+public class Animal {
+    private int numeroPatas;
+    private String especie;
+    private double tmaño;
+    private String nombre;
+
+    public double getTmaño() {
+        return tmaño;
+    }
+
+    public void setTmaño(double tmaño) {
+        this.tmaño = tmaño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+            
+    public static void main(String[] args) { 
+    }
+        public void hacerRuido(){
+            
+        }
+
+    public int getNumeroPatas() {
+        return numeroPatas;
+    }
+
+    public void setNumeroPatas(int numeroPatas) {
+        this.numeroPatas = numeroPatas;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+        
+        
+}
+  
+
---
+++ src/clase2/aula/objetos/Zoologico.java
@@ -0,0 +1,14 @@
+
+package clase2.aula.objetos;
+
+public class Zoologico {
+    
+    public static void main(String[] args) {
+        Animal tigre=new Animal();
+        //modificando el atributio
+        tigre.setNombre("Tigre");
+        //acediendo al atributo
+        System.out.println(tigre.getNombre());
+    }
+    
+}
