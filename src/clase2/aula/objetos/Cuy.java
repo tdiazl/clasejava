853d05184c09f9dc92d5985edac136b67a7696b5
PFR <PFR@LAB605-04>
2013-08-11 10:44:05 -0500

    Probando

--- src/clase2/aula/objetos/Cuy.java
+++
@@ -1,6 +0,0 @@
-package clase2.aula.objetos;
-public class Cuy {
-      public void hacerRuido(){
-            System.out.println("");
-        }
-}
--- src/clase2/aula/objetos/Zoologico.java
+++ src/clase2/aula/objetos/Zoologico.java
@@ -3,32 +3,31 @@ package clase2.aula.objetos;
 public class Zoologico {
 
     public static void main(String[] args) {
-        Zoologico.doHrencia();
+        Zoologico.doHerencia();
 
     }
-
-    public static void dpInstance() {
-        Animal tigre = new Animal();
-        //modificando el atributio
-        tigre.setNombre("Tigre");
-        //acediendo al atributo
-        System.out.println(tigre.getNombre());
-        tigre.hacerRuido();
-        
-        Gallina pepa=new Gallina();
-        pepa.setNumeroPatas(2);
-        System.out.println(pepa.getNumeroPatas());
-        pepa.hacerRuido();
-        
-
-    }
-
-    public static void doHrencia() {
+    
+    public static void doHerencia() {
         Tigre tigre = new Tigre();
         tigre.setNumeroPatas(4);
         tigre.hacerRuido();
         System.out.println(tigre.getNumeroPatas());
         tigre.hacerRuido(true);
         
+        Gallina pepa=new Gallina();
+        pepa.setNumeroPatas(2);
+        System.out.println(pepa.getNumeroPatas());
+        pepa.hacerRuido();  
     }
+
+    public static void dopInstance() {
+        Animal tigre = new Animal();
+        //modificando el atributio
+        tigre.setNombre("Tigre");
+        //acediendo al atributo
+        System.out.println(tigre.getNombre());
+         
+
+    }
+   
 }
